798668a5d801767bce566402e36910142436524f
dodio12138 <1159715241@qq.com>
2023-08-31 14:12:29 +0100

    main

---
+++ C/graph/random-work/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+#define HIGHT 100
+#define WIDTH 100
+
+struct cro
+{
+    int x;
+    int y;
+};
+
+struct cro loc = {HIGHT / 2, WIDTH / 4};
+void gotoxy(int x, int y);
+void hide(int k);
+int randomChoose();
+void loop();
+void ini();
+int speed = 5;
+char c = '+';
+// char* userInput;
+// char userc;
+
+int main()
+{
+    // printf("Enter walk speed(ms)\n");
+    // // 使用 scanf 获取用户输入的字符
+    // scanf("%s", userInput);
+
+    // speed = atoi(userInput);
+
+    // printf("Enter show char\n");
+    // // 使用 scanf 获取用户输入的字符
+    // scanf("%s", userc);
+
+    // // 将字符数组转换为整数
+
+    // c = userc;
+
+    ini();
+    loop();
+    return 0;
+}
+
+void ini()
+{
+    system("title Random Walk");
+    system("mode con cols=100 lines=50");
+    hide(1);
+}
+
+void loop()
+{
+    while (1)
+    {
+        gotoxy(loc.x, loc.y);
+        hide(1);
+        printf("%c", c);
+        Sleep(speed);
+        int dir = randomChoose();
+        switch (dir)
+        {
+        case 0:
+            loc.x += 1;
+            break;
+        case 1:
+            loc.x -= 1;
+            break;
+        case 2:
+            loc.y += 1;
+            break;
+        case 3:
+            loc.y -= 1;
+            break;
+
+        default:
+            break;
+        }
+        if (loc.x <= 0 || loc.y <= 0)
+        {
+            loc.x += 2;
+            loc.y += 2;
+        }
+        if (loc.x > 100)
+        {
+            loc.x -= 2;
+        }
+        if (loc.y > 50)
+        {
+            loc.y -= 2;
+        }
+        gotoxy(loc.x, loc.y);
+        hide(0);
+    }
+}
+
+int randomChoose()
+{
+    // 种子初始化
+    srand(time(NULL) + rand());
+    int dir = rand() % 4;
+    // printf("%d", dir);
+
+    return dir;
+}
+
+void gotoxy(int x, int y) // 移动光标
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void hide(int k) // 隐藏光标
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cci;
+    GetConsoleCursorInfo(hOut, &cci);
+    cci.bVisible = k; // 赋1为显示，赋0为隐藏
+    SetConsoleCursorInfo(hOut, &cci);
+}
